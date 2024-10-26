a24f8a526daf5d48981c3e3116a502ff5c01f61d
Huy Nguyen <huynq.b22cn391@stu.ptit.edu.vn>
2024-10-26 09:29:07 +0700

    api my favourite

--- app/routes/favorites_route.py
+++ app/routes/favorites_route.py
@@ -5,7 +5,6 @@ from app.models.folder_model import Folder
 from app.models.user_folder_model import User_Folder
 from app.schemas.api_response import APIResponse
 from ..schemas.token_schema import TokenData
-from ..schemas.favorite_schema import FavoriteResponse
 from ..utils.oauth2 import get_current_user
 
 router = APIRouter(
@@ -14,7 +13,6 @@ router = APIRouter(
 )
 
 
-# Endpoint thêm folder vào favorites
 @router.put("/add/{id}", response_model=APIResponse)
 def add_favorite(id: int,
                  current_user: TokenData = Depends(get_current_user),
@@ -23,20 +21,20 @@ def add_favorite(id: int,
     folder = db.query(Folder).filter(Folder.id == id).first()
     if not folder:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail= APIResponse(
-                                code = 1002,
-                                message = "Folder not found",
-                                err_message = "Folder not found"))
+                            detail= {
+                                "code": 1002,
+                                "message": "Folder not found",
+                            })
 
     # Kiểm tra nếu folder đã trong favorites của người dùng
     existing_favorite = db.query(User_Folder).filter(User_Folder.user_id == current_user.user_id,
                                                   User_Folder.folder_id == id).first()
     if existing_favorite:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail=APIResponse(
-                                code = 1002,
-                                message = "Favourite is already exist",
-                                err_message = "Favourite is already exist"))
+                            detail={
+                                "code": 1002,
+                                "message": "Folder already in favorites",
+                            })
 
     # Thêm folder vào favorites
     new_favorite = User_Folder(user_id=current_user.user_id, folder_id=id)
@@ -45,10 +43,9 @@ def add_favorite(id: int,
 
     return APIResponse(
         code=1000,
-        result={"result": f"add to favourite successfully"}
+        result={"result": "add to favourite successfully"}
     )
 
-# Endpoint xóa folder khỏi favorites
 @router.delete("/delete/{id}", response_model=APIResponse)
 def delete_favorite(id: int,
                     current_user: TokenData = Depends(get_current_user),
@@ -58,10 +55,10 @@ def delete_favorite(id: int,
                                          User_Folder.folder_id == id).first()
     if not favorite:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=APIResponse(
-                                code=1002,
-                                message="Favourite not found",
-                                err_message="Favourite not found"))
+                            detail= {
+                                "code": 1002,
+                                "message": "Folder not found",
+                            })
 
     # Xóa folder khỏi favorites
     db.delete(favorite)
@@ -72,28 +69,43 @@ def delete_favorite(id: int,
         result={"result": f"Folder with id {id} has been removed from favorites"}
     )
 
-# Endpoint lấy danh sách folder đã yêu thích
 @router.get("", response_model=APIResponse)
-def get_favorites(current_user: TokenData = Depends(get_current_user),
+def get_favorites(page:int = 1, limit: int = 8, current_user: TokenData = Depends(get_current_user),
                   db: Session = Depends(get_db)):
     # Lấy tất cả các folder mà người dùng đã yêu thích
-    favorites = db.query(User_Folder).filter(User_Folder.user_id == current_user.user_id).all()
+    skip = (page - 1) * limit
+
+    favorites = db.query(User_Folder).filter(User_Folder.user_id == current_user.user_id).offset(skip).limit(limit).all()
 
     if not favorites:
-        return APIResponse(
-            code=200,
-            result={"message": "No favorites found"}
-        )
-
-    # Tạo danh sách các folder từ favorites
-    favorite_list = [
-        FavoriteResponse(
-            folder_id=favorite.folder_id,
-            user_id=favorite.user_id
-        ) for favorite in favorites
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail= {
+                                "code": 1002,
+                                "message": "No folder found in favorites",
+                            })
+    
+    total = db.query(User_Folder).filter(User_Folder.user_id == current_user.user_id).count()
+    
+    data = [
+        {
+            "id": favorite.folder.id,
+            "name": favorite.folder.name,
+            "slug": favorite.folder.slug,
+            "star": favorite.folder.star,
+            "view": favorite.folder.view,
+            "author": {
+                "id": favorite.folder.author.id,
+                "username": favorite.folder.author.username,
+                "email": favorite.folder.author.email,
+            }
+        } for favorite in favorites
     ]
+    
 
     return APIResponse(
-        code=200,
-        result=favorite_list
+        code=1000,
+        result={
+            "total": total,
+            "items": data
+        }  
     )
